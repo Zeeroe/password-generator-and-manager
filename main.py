0e5016a7c3ac640da57164c907514c03e8cf78b9
Zeeroe <13524382+Zeeroe@users.noreply.github.com>
2021-02-23 16:37:05 -0500

    Update main.py
    User can now switch between settings for different password generation types (password or passphrase).
    Password length can be modified using a slider.
    User can change the number of words and the separator for passphrases.

--- main.py
+++ main.py
@@ -11,20 +11,22 @@ def gen_word():
         password = ''.join(random.choice(S.characters) for _ in range(S.length))
     except IndexError:
         password = ''
-    pw_entry.delete(0, len(pw_entry.get()))
-    pw_entry.insert(1, password)
+    text_entry.delete(0, len(text_entry.get()))
+    text_entry.insert(1, password)
 
 
 def gen_phrase():
     try:
-        passphrase = S.separator.join(rw.get_random_words(limit=S.words,
-                                                          minLength=S.minlength,
-                                                          maxLength=S.maxlength,
-                                                          hasDictionaryDef='True'))
+        sep = S.sep
+        passphrase = sep.join(rw.get_random_words(
+            limit=S.words,
+            minLength=S.minlength,
+            maxLength=S.maxlength,
+            hasDictionaryDef='True'))
 
         passphrase = passphrase.title()
-        pw_entry.delete(0, len(pw_entry.get()))
-        pw_entry.insert(1, passphrase)
+        text_entry.delete(0, len(text_entry.get()))
+        text_entry.insert(0, passphrase)
 
     except TypeError:
         print('TypeError')
@@ -36,29 +38,56 @@ root = tk.Tk()
 root.title("1Pass")
 root.geometry('300x400')
 
-# Text
-pw_entry = tk.Entry(root, width=50, justify=tk.CENTER)
-pw_entry.grid(column=0, row=0)
+text_entry = tk.Entry(root, width=50, justify=tk.CENTER)
+text_entry.grid(column=0, row=0)
 
-# Settings Label
 settings_label = tk.Label(text='Settings')
 settings_label.grid(column=0, row=2)
+
+
+# Dropdown Menu
+def switch_mode(mode):
+    if mode == 'Passphrase':
+        pw_frame.grid_forget()
+        pp_frame.grid(column=0, row=4)
+    elif mode == 'Password':
+        pp_frame.grid_forget()
+        pw_frame.grid(column=0, row=4)
+
+
+mode_list = ['Password', 'Passphrase']
+mode_str = tk.StringVar(root, value='Password')
+mode_drop = tk.OptionMenu(root, mode_str, *mode_list, command=switch_mode)
+mode_drop.grid(column=0, row=3)
 
 # Password Frame
 pw_frame = tk.Frame(root)
 pw_frame.grid(column=0, row=4)
 
 
+def length_f(v):
+    S.length = int(v)
+
+
+length_label = tk.Label(pw_frame, text='Length')
+length_label.grid(column=0, row=0)
+length_scale = tk.Scale(pw_frame, from_=4, to=32, orient=tk.HORIZONTAL, command=length_f)
+length_scale.set(S.length)
+length_scale.grid(column=1, row=0)
+
+
 class Checkbox:
     def __init__(self, lt, g, sub, add):
-        self.lt = lt
-        self.g = g
-        self.sub = sub
-        self.add = add
+        self.lt = lt  # Label Text
+        self.g = g  # Grid Row
+        self.sub = sub  # Characters to remove (Regex)
+        self.add = add  # Characters to add
 
         self.label = tk.Label(pw_frame, text=self.lt).grid(column=0, row=g)
         self.var = tk.IntVar(pw_frame)
-        self.check = tk.Checkbutton(pw_frame, variable=self.var, command=self.func).grid(column=1, row=g)
+        self.check = tk.Checkbutton(pw_frame, variable=self.var, command=self.func)
+        self.check.grid(column=1, row=g)
+        self.check.invoke()
 
     def func(self):
         if self.var.get() == 0:
@@ -67,27 +96,45 @@ class Checkbox:
             S.characters += self.add
 
 
-low = Checkbox('a-z', 0, '[a-z]', 'abcdefghijklmnopqrstuvwxyz')
-up = Checkbox('A-Z', 1, '[A-Z]', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-num = Checkbox('0-9', 2, '[0-9]', '0123456789')
-sym = Checkbox('!@#$%^&*', 3, '[!@#$%^&*]', '!@#$%^&*')
+low = Checkbox('a-z', 1, '[a-z]', 'abcdefghijklmnopqrstuvwxyz')
+up = Checkbox('A-Z', 2, '[A-Z]', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+num = Checkbox('0-9', 3, '[0-9]', '0123456789')
+sym = Checkbox('!@#$%^&*', 4, '[!@#$%^&*]', '!@#$%^&*')
 
 # Passphrase Frame
 pp_frame = tk.Frame(root)
-pp_frame.grid(column=0, row=4)
 
 
-# Dropdown Menu
-def switch_mode(v):
-    print(v)
+def words_f():
+    S.words = int(words_var.get())
 
 
-mode_list = ['Password', 'Passphrase']
-mode_str = tk.StringVar(root)
-mode_drop = tk.OptionMenu(root, mode_str, *mode_list, command=switch_mode)
-mode_drop.grid(column=0, row=3)
+words_label = tk.Label(pp_frame, text='Number of Words')
+words_label.grid(column=0, row=0)
+words_var = tk.IntVar(value=S.words)
+words_spinbox = tk.Spinbox(pp_frame, from_=1, to=16, width=2, state='readonly', textvariable=words_var, command=words_f)
+words_spinbox.grid(column=1, row=0)
 
 
+def callback(v):
+    if len(v) <= 1:
+        S.sep = v
+        return True
+    else:
+        return False
+
+
+reg = root.register(callback)
+
+sep_label = tk.Label(pp_frame, text='Separator')
+sep_label.grid(column=0, row=1)
+sep_var = tk.StringVar(value=S.sep)
+sep_entry = tk.Entry(pp_frame, width=2, validate="key", validatecommand=(reg, '%P'))
+sep_entry.grid(column=1, row=1)
+sep_entry.insert(0, S.sep)
+
+
+# Generate Button
 def generate():
     if mode_str.get() == 'Password':
         gen_word()
@@ -95,7 +142,8 @@ def generate():
         gen_phrase()
 
 
-generate_button = tk.Button(root, text="Generate Password", command=generate)
+generate_button = tk.Button(root, text="Regenerate Password", command=generate)
 generate_button.grid(column=0, row=1)
+generate_button.invoke()
 
 root.mainloop()
