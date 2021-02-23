a4d1cd4b6da34f4a1d956d4f685f0d5a6903402b
Zeeroe <13524382+Zeeroe@users.noreply.github.com>
2021-02-22 20:10:35 -0500

    Update gui.py
    Added a dropdown menu for picking whether to create a password or passphrase. Both use the same button and text box now.
    Changed some variable names so it's more uniform.
    Added some blank UI elements and a (currently empty) function which will be updated later to allow for switching between different settings for generating passwords.

--- gui.py
+++ gui.py
@@ -1,57 +1,74 @@
-from tkinter import *
+import tkinter as tk
 from settings import Settings
 import random, string, random_word
 
 S = Settings()
 rw = random_word.RandomWords()
 
-# Window
-window = Tk()
-window.title("1Pass")
-window.geometry('400x200')
 
-# Text
-entry_pw = Entry(window, width=32)
-entry_pw.grid(column=1, row=0)
-entry_pw.focus()
-
-entry_pp = Entry(window, width=32)
-entry_pp.grid(column=1, row=1)
-entry_pp.focus()
-
-# Label
-lbl_pw = Label(window, width=12, text="Password:")
-lbl_pw.grid(column=0, row=0)
-
-lbl_pp = Label(window, width=12, text="Passphrase:")
-lbl_pp.grid(column=0, row=1)
-
-
-def generate_password():
+def gen_word():
     password = ''.join(random.choice(S.characters) for _ in range(S.length))
-    entry_pw.delete(0, len(entry_pw.get()))
-    entry_pw.insert(1, password)
+    pw_entry.delete(0, len(pw_entry.get()))
+    pw_entry.insert(1, password)
 
 
-def generate_passphrase():
+def gen_phrase():
     try:
-        passphrase = S.separator.join(rw.get_random_words(limit=S.limit,
+        passphrase = S.separator.join(rw.get_random_words(limit=S.words,
                                                           minLength=S.minlength,
                                                           maxLength=S.maxlength,
                                                           hasDictionaryDef='True'))
 
-        passphrase = passphrase.lower()
-        entry_pp.delete(0, len(entry_pp.get()))
-        entry_pp.insert(1, passphrase)
+        passphrase = passphrase.title()
+        pw_entry.delete(0, len(pw_entry.get()))
+        pw_entry.insert(1, passphrase)
 
     except TypeError:
         print('TypeError')
-        generate_passphrase()
+        gen_phrase()
 
 
-btn = Button(window, text="Generate Password", command=generate_password)
-btn.grid(column=2, row=0)
-btn2 = Button(window, text="Generate Passphrase", command=generate_passphrase)
-btn2.grid(column=2, row=1)
+# Window
+root = tk.Tk()
+root.title("1Pass")
+root.geometry('300x400')
 
-window.mainloop()
+# Text
+pw_entry = tk.Entry(root, width=50, justify=tk.CENTER)
+pw_entry.grid(column=0, row=0)
+
+# Settings Label
+settings_label = tk.Label(text='Settings')
+settings_label.grid(column=0, row=2)
+
+# Password Frame
+pw_frame = tk.Frame(root)
+pw_frame.grid(column=0, row=4)
+
+# Passphrase Frame
+pp_frame = tk.Frame(root)
+pp_frame.grid(column=0, row=4)
+
+
+def switch_mode(v):
+    print(v)
+
+
+# Dropdown Menu
+mode_list = ['Password', 'Passphrase']
+mode_str = tk.StringVar(root)
+mode_drop = tk.OptionMenu(root, mode_str, *mode_list, command=switch_mode)
+mode_drop.grid(column=0, row=3)
+
+
+def generate():
+    if mode_str.get() == 'Password':
+        gen_word()
+    elif mode_str.get() == 'Passphrase':
+        gen_phrase()
+
+
+generate_button = tk.Button(root, text="Generate Password", command=generate)
+generate_button.grid(column=0, row=1)
+
+root.mainloop()
