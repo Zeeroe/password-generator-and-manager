ce15f7ca97324c7f7da75a67809920c8df11a9fa
Zeeroe <13524382+Zeeroe@users.noreply.github.com>
2021-03-24 00:33:54 -0400

    Update pwPanel.py
    Made it so if there is only one checkbox checked, it cannot be unchecked.
    This way, the user cannot uncheck all boxes and generate an empty password.
    If the generated password doesn't contain at least one character from each category that's checked, it will add a character from the category.

--- pwPanel.py
+++ pwPanel.py
@@ -1,29 +1,34 @@
 import tkinter as tk
 import random_word
-import re
+import random
 
 rw = random_word.RandomWords()
 
 
 class Checkbox:
-    def __init__(self, so, parent, g, sub, add):
-        self.so = so  # Settings Object
-        self.sub = sub  # Characters to remove (Regex)
-        self.add = add  # Characters to add
+    def __init__(self, panel, g, txt, ch):
+        self.panel = panel  # pwPanel
+        self.frame = self.panel.frame  # pwFrame
+        self.S = self.panel.S  # Settings Object
+        self.ch = ch
 
-        text = sub[1:-1]
+        self.label = tk.Label(self.frame, text=txt)
+        self.label.grid(column=0, row=g)
 
-        self.label = tk.Label(parent, text=text).grid(column=0, row=g)
-        self.var = tk.IntVar(parent)
-        self.check = tk.Checkbutton(parent, variable=self.var, command=self.func)
+        self.var = tk.IntVar(self.panel.frame)
+        self.check = tk.Checkbutton(self.frame, variable=self.var, command=self.func)
+        self.check.select()
         self.check.grid(column=1, row=g)
-        self.check.invoke()
 
     def func(self):
         if self.var.get() == 0:
-            self.so.characters = re.sub(self.sub, '', self.so.characters)
+            if self.panel.last_box():
+                self.S.characters[self.ch + '-'] = self.S.characters.pop(self.ch)
+            else:
+                self.check.select()
+
         elif self.var.get() == 1:
-            self.so.characters += self.add
+            self.S.characters[self.ch] = self.S.characters.pop(self.ch + '-')
 
 
 class pwPanel:
@@ -37,10 +42,51 @@ class pwPanel:
         self.length_scale.set(self.S.length)
         self.length_scale.grid(column=1, row=0)
 
-        self.low = Checkbox(self.S, self.frame, 1, '[a-z]', 'abcdefghijklmnopqrstuvwxyz')
-        self.up = Checkbox(self.S, self.frame, 2, '[A-Z]', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        self.num = Checkbox(self.S, self.frame, 3, '[0-9]', '0123456789')
-        self.sym = Checkbox(self.S, self.frame, 4, '[!@#$%^&*]', '!@#$%^&*')
+        self.low = Checkbox(self, 1, 'a-z', 'a')
+        self.upp = Checkbox(self, 2, 'A-Z', 'A')
+        self.num = Checkbox(self, 3, '0-9', '1')
+        self.sym = Checkbox(self, 4, '!@#$%^&*', '!')
+
+    def last_box(self):
+        if self.low.var.get() == 0 and \
+           self.upp.var.get() == 0 and \
+           self.num.var.get() == 0 and \
+           self.sym.var.get() == 0:
+            return False
+        else:
+            return True
+
+    def gen_word(self, te):
+        try:
+            pool = ''
+            for key in self.S.characters.keys():
+                if '-' not in key:
+                    pool += (self.S.characters[key])
+
+            password = ''.join(random.choice(pool) for _ in range(self.S.length))
+
+            # Ensuring the string has at least 1 character from each category
+            for key in self.S.characters.keys():
+                if '-' not in key:
+                    check = False
+                    for character in self.S.characters[key]:
+                        if character in password:
+                            check = True
+                            break
+                    if not check:
+                        password = password[1:-1] \
+                                   + random.choice(self.S.characters[key]) \
+                                   + password[0]
+
+            _ = random.sample(list(password), k=len(password))
+            password = ''.join(_)
+
+        except IndexError:
+            print('IndexError')
+            password = ''
+
+        te.delete(0, len(te.get()))
+        te.insert(1, password)
 
     def length_f(self, v):
         self.S.length = int(v)
