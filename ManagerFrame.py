e3a0d9ec5ca3092b1288039a55c318fc1613e283
Zeeroe <13524382+Zeeroe@users.noreply.github.com>
2021-04-25 01:05:10 -0400

    Update ManagerFrame.py

--- ManagerFrame.py
+++ ManagerFrame.py
@@ -2,48 +2,81 @@ import tkinter as tk
 import csv
 
 
-class Login:
-    def __init__(self, login_list, row, site='Website', user='Username', pw='Password'):
+class LoginObj:
+    def __init__(self, login_list, root, row, site, user, pw):
         self.frame = tk.Frame(login_list, bd=1, relief=tk.RIDGE)
         self.frame.grid(column=0, row=row)
+        self.root = root
 
         self.site = site
         self.user = user
         self.pw = pw
 
-        self.label_frame = tk.Frame(self.frame)
-        self.label_frame.grid(column=0, row=0)
+        self.edit_mode = False
 
-        self.site_label = tk.Label(self.label_frame, width=28, height=1, text=self.site)
-        self.site_label.grid(column=0, row=0)
+        self.entry_frame = tk.Frame(self.frame)
+        self.entry_frame.grid(column=0, row=0)
 
-        self.user_label = tk.Label(self.label_frame, width=28, height=1, text=self.user)
-        self.user_label.grid(column=0, row=1)
+        self.site_entry = tk.Entry(self.entry_frame, width=33)
+        self.site_entry.insert(0, self.site)
+        self.site_entry.grid(column=0, row=0)
+
+        self.user_entry = tk.Entry(self.entry_frame, width=33)
+        self.user_entry.insert(0, self.user)
+        self.user_entry.grid(column=0, row=1)
+
+        self.pw_entry = tk.Entry(self.entry_frame, width=33)
+        self.pw_entry.insert(0, self.pw)
+
+        self.list_fields = [self.site_entry, self.user_entry, self.pw_entry]
+
+        for field in self.list_fields:
+            field.configure(state=tk.DISABLED)
 
         self.edit_button = tk.Button(self.frame, text='E', width=2, command=self.edit)
-        self.edit_button.grid(column=1, row=0)
+        self.edit_button.grid(column=1, row=0, sticky='n')
 
         self.copy_button = tk.Button(self.frame, text='C', width=2, command=self.copy)
-        self.copy_button.grid(column=2, row=0)
+        self.copy_button.grid(column=2, row=0, sticky='n')
 
         self.delete_button = tk.Button(self.frame, text='D', width=2, command=self.delete)
-        self.delete_button.grid(column=3, row=0)
+        self.delete_button.grid(column=3, row=0, sticky='n')
 
     def edit(self):
-        print('E ' + self.site + ' ' + self.user)
+        if not self.edit_mode:
+            for field in self.list_fields:
+                field.configure(state=tk.NORMAL)
+            self.pw_entry.grid(column=0, row=2)
+            self.edit_mode = True
+        else:
+            for field in self.list_fields:
+                field.configure(state=tk.DISABLED)
+            self.pw_entry.grid_forget()
+            self.edit_mode = False
 
     def copy(self):
-        print('C ' + self.site + ' ' + self.user)
+        self.root.clipboard_clear()
+        self.root.clipboard_append(self.pw_entry.get())
 
     def delete(self):
-        print('D ' + self.site + ' ' + self.user)
+        self.frame.grid_forget()
+        logins_file = open('logins.txt', 'r')
+        list_logins = list(csv.reader(logins_file, delimiter=','))
+        list_logins.remove([self.site, self.user, self.pw])
+        logins_file.close()
+
+        logins_file = open('logins.txt', 'w', newline='')
+        x = csv.writer(logins_file, delimiter=',')
+        x.writerows(list_logins)
+        logins_file.close()
 
 
 class ManagerFrame:
-    def __init__(self, root):
-        self.frame = tk.Frame(root)
+    def __init__(self, frame, root):
+        self.frame = tk.Frame(frame)
+        self.root = root
 
-        self.list_logins = []
+        self.list_logins_obj = []
 
         self.top_frame = tk.Frame(self.frame)
         self.top_frame.grid(row=0)
@@ -51,12 +84,12 @@ class ManagerFrame:
         self.bottom_frame.grid(column=0, row=1)
 
         self.add_button = tk.Button(self.top_frame, width=10, text='Add Login', command=self.add_login)
-        self.add_button.grid(column=0, row=0)
+        self.add_button.grid(column=0, row=0, sticky='w')
 
-        self.search = tk.Entry(self.top_frame, width=50)
-        self.search.grid(column=0, row=1)
+        self.search = tk.Entry(self.top_frame, width=49)
+        self.search.grid(column=0, row=1, sticky='w')
 
-        self.canvas = tk.Canvas(self.bottom_frame, width=275, height=310)
+        self.canvas = tk.Canvas(self.bottom_frame, width=275, height=310, relief=tk.FLAT)
         self.canvas.grid(column=0, row=0)
         self.canvas.bind_all("<MouseWheel>", self._on_mouse_wheel)
 
@@ -68,15 +101,7 @@ class ManagerFrame:
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.canvas.create_window((0, 0), window=self.logins_frame, anchor='nw')
 
-        with open('logins.txt', 'r') as logins_file:
-            logins = csv.reader(logins_file, delimiter=',')
-            h = 0
-            row = 0
-            for login in logins:
-                self.list_logins.append(Login(self.logins_frame, row, login[0], login[1], login[2]))
-                row += 1
-                h += 44
-                self.logins_frame.configure(height=h)
+        self.import_logins()
 
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
@@ -85,3 +110,15 @@ class ManagerFrame:
 
     def _on_mouse_wheel(self, event):
         self.canvas.yview_scroll(-1 * int((event.delta / 120)), "units")
+
+    def import_logins(self):
+        logins_file = open('logins.txt', 'r')
+        list_logins = list(csv.reader(logins_file, delimiter=','))
+        h = 0
+        row = 0
+        for login in list_logins:
+            self.list_logins_obj.append(LoginObj(self.logins_frame, self.root, row, login[0], login[1], login[2]))
+            row += 1
+            h += 44
+            self.logins_frame.configure(height=h)
+        logins_file.close()
