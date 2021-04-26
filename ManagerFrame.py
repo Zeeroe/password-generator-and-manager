e6265686d3e89f8d15248b3e20cf28ef57f3e7e9
Zeeroe <13524382+Zeeroe@users.noreply.github.com>
2021-04-25 22:19:28 -0400

    Update ManagerFrame.py

--- ManagerFrame.py
+++ ManagerFrame.py
@@ -1,5 +1,8 @@
 import tkinter as tk
-import csv
+import pyAesCrypt
+import json
+
+bufferSize = 256 * 1024
 
 
 class LoginObj:
@@ -7,6 +10,7 @@ class LoginObj:
         self.frame = tk.Frame(mger.logins_frame, bd=1, relief=tk.RIDGE)
         self.frame.grid(column=0, row=row)
 
+        self.mger = mger
         self.logins_frame = mger.logins_frame
         self.canvas = mger.canvas
         self.root = mger.root
@@ -60,10 +64,10 @@ class LoginObj:
             for field in self.list_fields:
                 field.configure(state=tk.DISABLED)
                 new.append(field.get())
-            index = self.list_logins.index([self.website, self.user, self.pw])
-            self.list_logins[index] = new  # update login information
+            index = self.list_logins[0]['logins'].index([self.website, self.user, self.pw])
+            self.list_logins[0]['logins'][index] = new  # update login information
             [self.website, self.user, self.pw] = new
-            self.update_file()
+            self.mger.encrypt()
 
             self.pw_entry.grid_forget()
             self.logins_frame.configure(height=(h-20))
@@ -76,36 +80,67 @@ class LoginObj:
 
     def delete(self):
         self.frame.grid_forget()
-        self.list_logins.remove([self.website, self.user, self.pw])
-        self.update_file()
+        self.list_logins[0]['logins'].remove([self.website, self.user, self.pw])
+        self.mger.encrypt(self.mger.key)
         h = self.logins_frame.winfo_height()
         self.logins_frame.configure(height=(h - 40))
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-    def update_file(self):
-        logins_file = open('logins.txt', 'w', newline='')
-        writer = csv.writer(logins_file, delimiter=',')
-        writer.writerows(self.list_logins)
-        logins_file.close()
+
+class LockFrame:
+    def __init__(self, mger):
+        self.mger = mger
+        self.frame = tk.Frame(mger.frame)
+        self.frame.grid(column=0, row=0)
+
+        self.reg = self.frame.register(self.validate_cmd)
+
+        self.keycode_entry = tk.Entry(self.frame, width=5, validate="key", validatecommand=(self.reg, '%P'))
+        self.keycode_entry.grid(column=0, row=0)
+
+        self.unlock_button = tk.Button(self.frame, text='Unlock', command=self.unlock_f)
+        self.unlock_button.grid(column=0, row=1)
+
+    def validate_cmd(self, v):
+        if len(v) <= 4: return True
+        else: return False
+
+    def unlock_f(self):
+        keycode = self.keycode_entry.get()
+        if len(keycode) == 4:
+            self.mger.decrypt(keycode)
+            print(self.mger.list_logins)
+            if len(self.mger.list_logins) != 0 and self.mger.list_logins[0]['key'] == keycode:
+                print(self.mger.key)
+                self.mger.key = keycode
+                self.mger.import_logins()
+                self.frame.grid_forget()
+                self.mger.top_frame.grid(row=0, sticky='w')
+                self.mger.bottom_frame.grid(row=1)
 
 
 class ManagerFrame:
-    def __init__(self, frame, root):
-        self.frame = tk.Frame(frame)
+    def __init__(self, parent, root):
+        self.frame = tk.Frame(parent)
         self.root = root
 
-        self.list_logins_obj = []
+        self.key = ""
+
+        self.list_logins = []
+
+        self.locked = True
+        self.lock_frame = LockFrame(self)
 
         self.top_frame = tk.Frame(self.frame)
-        self.top_frame.grid(row=0)
         self.bottom_frame = tk.Frame(self.frame)
-        self.bottom_frame.grid(column=0, row=1)
+
+        self.search_label = tk.Label(self.top_frame, text='Search')
+        self.search_label.grid(column=0, row=0, sticky='w')
+        self.search_entry = tk.Entry(self.top_frame, width=40)
+        self.search_entry.grid(column=1, row=0, sticky='w')
 
         self.add_button = tk.Button(self.top_frame, width=10, text='Add Login', command=self.add_login)
-        self.add_button.grid(column=0, row=0, sticky='w')
-
-        self.search = tk.Entry(self.top_frame, width=49)
-        self.search.grid(column=0, row=1, sticky='w')
+        self.add_button.grid(column=0, row=1, columnspan=2, sticky='w')
 
         self.canvas = tk.Canvas(self.bottom_frame, width=275, height=310, relief=tk.FLAT)
         self.canvas.grid(column=0, row=0)
@@ -119,16 +154,14 @@ class ManagerFrame:
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.canvas.create_window((0, 0), window=self.logins_frame, anchor='nw')
 
-        self.logins_file = open('logins.txt', 'r')
-        self.list_logins = list(csv.reader(self.logins_file, delimiter=','))
-        self.import_logins()
-
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        self.login_objects = []
 
     def add_login(self):
-        row = len(self.list_logins_obj) + 1
-        self.list_logins_obj.append(LoginObj(self, row, '', '', ''))
-        self.list_logins.append(['', '', ''])
+        row = len(self.login_objects) + 1
+        new_login = LoginObj(self, row, '', '', '')
+        new_login.edit_button.invoke()
+        self.login_objects.append(new_login)
+        self.list_logins[0]['logins'].append(['', '', ''])
         h = self.logins_frame.winfo_height()
         self.logins_frame.configure(height=(h + 40))
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
@@ -140,9 +173,34 @@ class ManagerFrame:
     def import_logins(self):
         h = 0
         row = 0
-        for login in self.list_logins:
-            self.list_logins_obj.append(LoginObj(self, row, login[0], login[1], login[2]))
+        for login in self.list_logins[0]['logins']:
+            self.login_objects.append(LoginObj(self, row, login[0], login[1], login[2]))
             row += 1
             h += 40
             self.logins_frame.configure(height=h)
-        self.logins_file.close()
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def decrypt(self, key):
+        try:
+            pyAesCrypt.decryptFile("logins.json.aes", "logins.json", key, bufferSize)
+            file = open('logins.json', 'r+')
+            read = file.read()
+            file.truncate(0)
+            file.close()
+
+            logins_new = json.loads(read)
+            self.list_logins = logins_new
+
+        except ValueError:
+            print('Wrong password (or file is corrupted)')
+
+    def encrypt(self):
+        file = open('logins.json', "w")
+        json.dump(self.list_logins, file, indent=4)
+        file.close()
+
+        pyAesCrypt.encryptFile("logins.json", "logins.json.aes", self.key, bufferSize)
+
+        file = open('logins.json', "w")
+        file.truncate(0)
+        file.close()
